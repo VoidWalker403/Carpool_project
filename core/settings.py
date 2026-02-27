5a776467d5f694d4be1a532f88bf8ff38777469f
Amit Jakhar <ajakhar246@gmail.com>
2026-02-27 17:46:20 +0530

    Prepare for Render deployment

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
 }
 DEBUG = False
 
-ALLOWED_HOSTS = ["your-app.onrender.com"]
+ALLOWED_HOSTS = ["VoidWalker403.pythonanywhere.com"]
 MIDDLEWARE = [
   "django.middleware.security.SecurityMiddleware",
   "whitenoise.middleware.WhiteNoiseMiddleware",
