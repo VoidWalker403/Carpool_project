03c2a85e30d101c796be80f19608070fe3c52c47
Amit Jakhar <ajakhar246@gmail.com>
2026-03-04 03:00:13 +0530

    Fix middleware and static settings

--- core/settings.py
+++ core/settings.py
@@ -136,9 +136,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["voidwalker403.pythonanywhere.com"]
 MIDDLEWARE = [
-  "django.middleware.security.SecurityMiddleware",
-  "whitenoise.middleware.WhiteNoiseMiddleware",
-  ...
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
